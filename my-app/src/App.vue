46d287c5fd84f6cd78fff325ba31e43158c1b9c7
bubajiu34 <91668786+bubajiu34@users.noreply.github.com>
2023-10-12 08:46:05 +0800

    Add files via upload

---
+++ my-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- <el-row>
+      <el-button disabled>默认按钮</el-button>
+      <el-button type="primary" disabled>主要按钮</el-button>
+      <el-button type="success" disabled>成功按钮</el-button>
+      <el-button type="info" disabled>信息按钮</el-button>
+      <el-button type="warning" disabled>警告按钮</el-button>
+      <el-button type="danger" disabled>危险按钮</el-button>
+    </el-row> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+<style lang="less">
+  html,body{
+    margin: 0;
+    padding: 0
+  }
+</style>
---
+++ my-app/src/views/goods.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <el-container>
+            <el-header class="custom-header"> <!-- 添加一个类名以便样式设置 -->
+                <goodsHeader />
+            </el-header>
+            <el-row>
+                <el-col :span="8">
+                    <div class="GAisde">
+                        <goods-aside />
+                    </div>
+                </el-col>
+                <el-col :span="16">
+                    <div class="GMain">
+                       <goods-test />
+                        <goods-test-2 />
+                    </div>
+                </el-col>    
+            </el-row>
+        </el-container>
+    </div>
+</template>
+<script>
+import GoodsAside from '@/components/goodsAside.vue'
+import goodsHeader from '../components/goodsHeader.vue'
+import GoodsTest from '@/components/goodsTest.vue'
+import GoodsTest2 from '@/components/goodsTest2.vue'
+export default {
+    data() {
+        return {}
+    },
+    components:{
+        goodsHeader,
+        GoodsAside,
+        GoodsTest,
+        GoodsTest2,
+    }
+}
+</script>
+<style scoped>
+.custom-header { /* 添加一个类名以便样式设置 */
+    margin: 0;
+    padding: 0;
+    border-bottom: 2px solid #000000; /* 添加底部边框样式 */
+}
+</style>
