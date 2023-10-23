6dc456ad286a4ffe15351f2a90dba9e717dc960f
Puppet17514 <1422647925@qq.com>
2023-10-23 18:28:32 +0800

    订单服务层测试

--- backend-jixian/src/test/java/com/rj/backendjixian/service/impl/OrderServiceImplTest.java
+++ backend-jixian/src/test/java/com/rj/backendjixian/service/impl/OrderServiceImplTest.java
@@ -1,6 +1,7 @@
-package com.rj.backendjixian.OrderServiceTests;
+package com.rj.backendjixian.service.impl;
 
 import com.rj.backendjixian.model.dto.GoodOrderDto;
+import com.rj.backendjixian.model.entity.BuyerEntity;
 import com.rj.backendjixian.model.entity.GoodOrderEntity;
 import com.rj.backendjixian.model.entity.OrderEntity;
 import com.rj.backendjixian.model.vo.HistoryOrderVo;
@@ -8,34 +9,52 @@ import com.rj.backendjixian.model.vo.Response;
 import com.rj.backendjixian.service.IGoodOrderService;
 import com.rj.backendjixian.service.IGoodService;
 import com.rj.backendjixian.service.IOrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 订单服务层测试
  */
+@Slf4j
 @SpringBootTest
-public class OrderServiceTest {
+@Transactional
+public class OrderServiceImplTest {
     @Autowired
-    IOrderService orderService;
+    private IOrderService orderService;
     @Autowired
-    IGoodService goodService;
+    private IGoodService goodService;
     @Autowired
-    IGoodOrderService goodOrderService;
+    private IGoodOrderService goodOrderService;
 
-    String shop_id = "630e807bdf604941b89192af2eb7396e";
-    GoodOrderDto goodOrderDto = new GoodOrderDto("630e807bdf604941b89192af2eb7396e","999eb2a9a68242589a504f6d24dfd5e7","7f975160dcd44768946149e52270ee54",20,200);
+    private final String shop_id = "630e807bdf604941b89192af2eb7396e";
+    private final GoodOrderDto goodOrderDto = new GoodOrderDto(
+            "630e807bdf604941b89192af2eb7396e","999eb2a9a68242589a504f6d24dfd5e7",
+            "7f975160dcd44768946149e52270ee54",20,200);
+
+    List<HistoryOrderVo> list1 = new ArrayList<>();
+
 
     /**
      * 测试查询历史订单
      */
     @Test
     void getHistoryOrdersTest(){
+        list1.add( new HistoryOrderVo("f05b183312f444f19c5299ee04e67eee","康师傅","1","2.png",
+                        "手动支付","e9656b856f874e838449fa88fcef60ac", new BuyerEntity("e9656b856f874e838449fa88fcef60ac",
+                        "test",null,"慈溪","13819442222",null)));
+        list1.add(new HistoryOrderVo("1f673d8967e342c29382d85d7a9a9a0d","香蕉2","0","1.png","手动支付",
+                        "7b573034c4b14fb28695b90869477366",new BuyerEntity("7b573034c4b14fb28695b90869477366",
+                        "test233",null,"杭州","23219442222",null)));
         List<HistoryOrderVo> list = orderService.getHistoryOrders(shop_id);
-        list.forEach(System.out::println);
+        Assertions.assertEquals(list1,list);
     }
 
     /**
@@ -44,9 +63,11 @@ public class OrderServiceTest {
     @Test
     void updateTest(){
         OrderEntity orderEntity = orderService.getById("f05b183312f444f19c5299ee04e67eee");
-        int status = 1;
+        int status = 0;
         orderEntity.setStatus(status);
-        System.out.println("影响的数据库记录数：  " + orderService.update(orderEntity));
+        orderService.update(orderEntity);
+        Assertions.assertEquals(status,orderService.getById("f05b183312f444f19c5299ee04e67eee").getStatus());
+
     }
 
 
@@ -61,6 +82,8 @@ public class OrderServiceTest {
         orderEntity.setShopId(goodOrderDto.getShopId());
         orderService.save(orderEntity);
 
+        String str = orderEntity.getId();
+
         GoodOrderEntity goodOrderEntity = new GoodOrderEntity();
         goodOrderEntity.setOrderId(orderEntity.getId());
         goodOrderEntity.setGoodId(goodOrderDto.getGoodId());
@@ -68,5 +91,7 @@ public class OrderServiceTest {
         goodOrderEntity.setSum(goodOrderDto.getSum());
 
         goodOrderService.save(goodOrderEntity);
+
+        Assertions.assertEquals(str,goodOrderEntity.getOrderId());
     }
 }
