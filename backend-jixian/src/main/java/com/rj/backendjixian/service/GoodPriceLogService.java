6fb6faf43e080f56ff98f22c44fa5fdb0f5898bf
tiantian <a2450804878@hotmail.com>
2023-11-02 08:58:35 +0800

    数据库增加good_price_log表管理价格，并生成相关crud代码

---
+++ backend-jixian/src/main/java/com/rj/backendjixian/service/GoodPriceLogService.java
@@ -0,0 +1,15 @@
+package com.rj.backendjixian.service;
+
+
+import com.rj.backendjixian.model.entity.GoodPriceLogEntity;
+import com.mybatisflex.core.service.IService;
+
+/**
+ * 服务层。
+ *
+ * @author mybatis-flex-helper automatic generation
+ * @since 1.0
+ */
+public interface GoodPriceLogService extends IService<GoodPriceLogEntity> {
+
+}
---
+++ backend-jixian/src/main/java/com/rj/backendjixian/service/impl/GoodPriceLogServiceImpl.java
@@ -0,0 +1,19 @@
+package com.rj.backendjixian.service.impl;
+
+
+import org.springframework.stereotype.Service;
+import com.rj.backendjixian.service.GoodPriceLogService;
+import com.rj.backendjixian.model.entity.GoodPriceLogEntity;
+import com.rj.backendjixian.mapper.GoodPriceLogMapper;
+import com.mybatisflex.spring.service.impl.ServiceImpl;
+
+/**
+ * 服务层实现。
+ *
+ * @author mybatis-flex-helper automatic generation
+ * @since 1.0
+ */
+@Service
+public class GoodPriceLogServiceImpl extends ServiceImpl<GoodPriceLogMapper, GoodPriceLogEntity> implements GoodPriceLogService {
+
+}
